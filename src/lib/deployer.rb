a2a28e7295ea9f4f3fd2be6909b16a81f95eb1ed
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2021-04-16 12:56:41 +0100

    Update deployer.rb

--- src/lib/deployer.rb
+++ src/lib/deployer.rb
@@ -1,7 +1,15 @@
 module D
 
   class Deployer
-
+    def self.deploy()
+      new.deploy()
+    end
+    
+    def deploy(project:, env_name:, username: ...)
+      puts "Deployment of #{project} - env: #{env_name} - triggered by: #{username} - starting"
+      
+      
+    end
   end
 
 end
