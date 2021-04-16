69a18c865464c25d5fa5c570986571b78b478429
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2021-04-16 12:55:51 +0100

    Update build.rb

--- src/models/build.rb
+++ src/models/build.rb
@@ -1,3 +1,23 @@
 class Build
 
+  def self.run(project:, containers:)
+    new.run(project: project, containers: containers)
+  end
+  
+  def run(project:, containers:)
+    puts "Building containers for project #{project}"
+    build_containers containers: containers
+  end
+    
+  def build_containers(containers:)
+    containers.each do |container_name|
+      puts "Building container #{container_name}"
+      build_container container_name: container_name
+    end
+  end
+  
+  def build_container(container_name:)
+    ssh_exe "docker-compose build"
+  end
+  
 end
