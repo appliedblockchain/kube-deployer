f409ff6687dd669df24b0912ece6fefb43ed55eb
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2021-04-18 12:32:05 +0100

    start work on push

--- src/lib/push.rb
+++ src/lib/push.rb
@@ -2,4 +2,22 @@
 
 class Push
 
+  def self.run
+    new.run
+  end
+  
+  def run
+  
+  end
+  
+  def push_all
+    cointainers.each do |container|
+      push_container container
+    end
+  end
+  
+  def push
+    ssh_exe "cd dir; docker-compose push"
+  end
+  
 end
