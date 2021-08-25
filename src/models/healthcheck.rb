4a4236481a6f4b97b21ccf42a9fac1e964eb7e7f
Francesco Canessa (makevoid) <makevoid@gmail.com>
2021-08-25 09:00:39 +0100

    address load balancer 502 - healthcheck

--- src/models/healthcheck.rb
+++ src/models/healthcheck.rb
@@ -1,4 +1,8 @@
+class HealthcheckFailed502 < RuntimeError; end
+
 class Healthcheck
+
+  RETRY_TIMEOUT = 8 # seconds - consider a deployment failed if the container(s) are not up after (8) seconds
 
   URL_API = "/api/health"
   URL_REACT = "/health"
@@ -45,9 +49,22 @@ class Healthcheck
   end
 
   def healthcheck_failing(url:)
+    time_start ||= Time.now
+    status = healthcheck_failing_http url: url
+    timer = Time.now - time_start
+    return "timeout-retry" if timer > RETRY_TIMEOUT
+    return status
+  rescue HealthcheckFailed502 => err
+    puts "healthcheck - got 502 - url: #{url}"
+    sleep 0.5
+    retry
+  end
+
+  def healthcheck_failing_http(url:)
     resp = http_get url: url
+    raise HealthcheckFailed502 if resp.status == 502
     return resp.status.to_s if resp.status != 200
-    return false
+    return false # healthcheck not
   rescue Excon::Error::Timeout, Excon::Error::Socket => err
     return "timeout"
   end
