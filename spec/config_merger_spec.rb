13f542828492954d94774dfded5e9dc9148053a0
Francesco Canessa (makevoid) <makevoid@gmail.com>
2021-08-26 08:31:19 +0100

    move tmp dir

--- spec/config_merger_spec.rb
+++ spec/config_merger_spec.rb
@@ -27,14 +27,25 @@ services:
     - B
   "
 
+  def tmp_path
+
+  end
+
   before :all do
-    YAML_SOURCE
+    File.write "#{PATH}/tmp/yaml_source.yml",   YAML_SOURCE
+    File.write "#{PATH}/tmp/yaml_override.yml", YAML_OVERRIDE
+    File.write "#{PATH}/tmp/yaml_output.yml",   YAML_OUTPUT
   end
 
   it "merges two yamls" do
+    source    = "#{PATH}/tmp/yaml_source.yml"
+    override  = "#{PATH}/tmp/yaml_override.yml"
+    output    = "#{PATH}/tmp/yaml_output.yml"
     merger = ConfigMerger.new
-    YAML
-    merger.merge_yml_file(source_file:, file_name:)
+    def merger.override_dir
+      "#{PATH}/tmp"
+    end
+    merger.merge_yml_file source_file: source, file_name: "yaml_source.yml"
   end
 
 end
