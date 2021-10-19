1d4b0b67156b2187a8d6fbeb2b4193afc82c5a31
Francesco Canessa (makevoid) <makevoid@gmail.com>
2021-10-19 18:06:35 +0100

    minor fixes

--- src/lib/kube.rb
+++ src/lib/kube.rb
@@ -1,14 +1,16 @@
 module Kube
   # TODO: exec kubectl commands
 
-  def pods
-    exe kube :get, :pods
-  end
+  # TODO: add declarative commands
 
-  def apply
-    exe kube :get, :pods
-    # exe kube :apply, "-f ...."
-  end
+  # def pods
+  #   exe kube :get, :pods
+  # end
+  #
+  # def apply
+  #   exe kube :get, :pods
+  #   # exe kube :apply, "-f ...."
+  # end
 
   def kube(command, resource, *args)
     puts "executing #{resource}.#{command}():"
